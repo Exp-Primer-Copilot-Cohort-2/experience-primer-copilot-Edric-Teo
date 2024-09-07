57b856dce998dacc69c5187ef367eeafbf6b11ad
Edric-Teo <160724070+Edric-Teo@users.noreply.github.com>
2024-09-07 17:18:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,11 @@
-// Create a web server that listens on port 3000 and returns a response with the text "Hello, this is a comment" when it receives a request to the /comments URL. The server should return a 404 Not Found response for all other URLs.
-function createServer() {
-    var server = http.createServer(function (req, res) {
-        if (req.url === '/comments') {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Hello, this is a comment');
-        } else {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('404 Not Found');
-        }
-    });
-    return server;
+// Create web server
+function createWeb (port) {
+    var express = require('express');
+    var app = express();
+    var bodyParser = require('body-parser');
+    var fs = require('fs');
+    var path = require('path');
+    var comments = require
 }
 
 
